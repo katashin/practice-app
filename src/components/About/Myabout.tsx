5f16a6d80bafdfc86fe94004822bf89a3df33255
katashin <katachanpe0513@icloud.com>
2022-01-10 04:42:11 +0900

    コメントの変更

--- src/components/About/Myabout.tsx
+++ src/components/About/Myabout.tsx
@@ -1,7 +1,7 @@
 export default function About(): JSX.Element {
   return (
     <>
-      <p className='text-3xl font-bold text-center bg-red-100'>
+      <p className='text-3xl font-bold text-center bg-blue-300'>
         片山真介です。宜しくお願いします。
       </p>
     </>
--- src/components/Skills/Myskill.tsx
+++ src/components/Skills/Myskill.tsx
@@ -2,9 +2,15 @@ export default function Skills(): JSX.Element {
   return (
     <>
       <ul className='text-xl font-bold text-gray-500'>
-        <li>睡眠時間は４時間でOK</li>
-        <li>好きなことに没頭できる</li>
-        <li>休憩はいらないタイプです</li>
+        <li className='border-b border-gray-400'>
+          スキル スキル スキル スキル スキル スキル スキル スキル
+        </li>
+        <li className='border-b border-gray-400'>
+          スキル スキル スキル スキル スキル スキル スキル スキル
+        </li>
+        <li className='border-b border-gray-400'>
+          スキル スキル スキル スキル スキル スキル スキル スキル
+        </li>
       </ul>
     </>
   );
